e8afbad24efe5711d911e163dfc2896b45b69128
Christoph von Krüchten <christoph@vonkruechten.de>
2019-09-15 00:08:43 +0200

    (haskell) ignore all tests

--- haskell/test/SafeSpacesSpec.hs
+++ haskell/test/SafeSpacesSpec.hs
@@ -3,32 +3,29 @@ module SafeSpacesSpec where
 import Test.Hspec
 import SafeSpaces (convertCoordinates, findSafeSpaces, adviceForAlex, SearchResult(..))
 
-main :: IO ()
-main = hspec spec
-
 spec :: Spec
 spec = do
     describe "Convert coordinates" $ do
-        it "returns empty numeric coordinates for empty alphanumeric coordinates" $ do
+        xit "returns empty numeric coordinates for empty alphanumeric coordinates" $ do
             convertCoordinates [] `shouldBe` []
         
-        it "converts a single coordinate" $ do
+        xit "converts a single coordinate" $ do
             convertCoordinates ["F3"] `shouldBe` [(5, 2)]
         
-        it "converts a list of coordinates to an array" $ do
+        xit "converts a list of coordinates to an array" $ do
             convertCoordinates ["B6","C2","J7"] `shouldBe` [(1, 5), (2, 1), (9, 6)]
 
-        it "handles alphanumeric coordinates with a double digit number" $ do
+        xit "handles alphanumeric coordinates with a double digit number" $ do
             convertCoordinates ["J10"] `shouldBe` [(9, 9)]
 
     describe "Find safe spaces in the city based on agent locations" $ do
-        it "returns correct result for six agents at specified locations" $ do
+        xit "returns correct result for six agents at specified locations" $ do
             findSafeSpaces [(1, 1), (3, 5), (4, 8), (7, 3), (7, 8), (9, 1)] `shouldMatchList` [(0, 9), (0, 7), (5, 0)]
             findSafeSpaces [(0, 0), (0, 9), (1, 5), (5, 1), (9, 0), (9, 9)] `shouldMatchList` [(5, 7), (6, 6), (7, 5)]
             findSafeSpaces [(0, 0)] `shouldMatchList` [(9, 9)]
 
     describe "Handle edge cases and offering recommendations" $ do
-        it "works for no safe locations" $ do
+        xit "works for no safe locations" $ do
             adviceForAlex [ "A1", "A2", "A3", "A4", "A5", "A6", "A7", "A8", "A9", "A10"
                             , "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B8", "B9", "B10"
                             , "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C8", "C9", "C10"
@@ -41,21 +38,21 @@ spec = do
                             , "J1", "J2", "J3", "J4", "J5", "J6", "J7", "J8", "J9", "J10"
                             ] `shouldBe` NoSafeSpaces
 
-        it "works for six agents at specified locations" $ do
+        xit "works for six agents at specified locations" $ do
             let SafeSpaces result = adviceForAlex [ "B2", "D6", "E9", "H4", "H9", "J2" ]
             result `shouldMatchList` [ "A10", "A8", "F1" ]
             
 
-        it "works for seven agents at specified locations" $ do
+        xit "works for seven agents at specified locations" $ do
             let SafeSpaces result = adviceForAlex [ "B4", "C4", "C8", "E2", "F10", "H1", "J6" ]
             result `shouldMatchList` [ "A1", "A10", "E6", "F5", "F6", "G4", "G5","G7", "H8", "I9", "J10" ]
 
-        it "works for six agents at different specified locations" $ do
+        xit "works for six agents at different specified locations" $ do
             let SafeSpaces result = adviceForAlex [ "A1", "A10", "B6", "F2", "J1", "J10" ]
             result `shouldMatchList` [ "F8", "G7", "H6" ]
 
-        it "works for only one agent" $ do
+        xit "works for only one agent" $ do
             adviceForAlex [ "A1" ] `shouldBe` SafeSpaces [ "J10" ]
 
-        it "works for agent outside the map (no agent in the city)" $ do
+        xit "works for agent outside the map (no agent in the city)" $ do
             adviceForAlex [ "A12" ] `shouldBe` WholeCityIsSafe
